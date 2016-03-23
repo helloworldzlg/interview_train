4e99d400cc351cd9d782d46928547568e646a677
helloworldzlg <helloworld_zlg@outlook.com>
2016-03-23 08:54:26 +0800

    测试当前编译的是C还是C++

---
+++ 测试当前编译的是C还是C++.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+ 
+#ifdef _cplusplus
+	#define LANGUAGE_FLAG  0
+#else
+	#define LANGUAGE_FLAG  1
+#endif
+
+int main()
+{
+	if (LANGUAGE_FLAG) 
+		printf("current language is C\n");
+	else
+		printf("current language is C++\n");
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
